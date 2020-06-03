af47edf0e5cfb8e37c76bfd601ccf8e39f3979f6
Maxi <maxipg@gmail.com>
2020-06-03 15:53:53 -0300

    Env Mongo

--- server/config/config.js
+++ server/config/config.js
@@ -18,7 +18,7 @@ let urlDB;
 if (process.env.NODE_ENV === 'dev') {
     urlDB = 'mongodb://localhost:27017/cafe';
 }else {
-    urlDB = 'mongodb+srv://MAXIGUISI:2EdogYKq4So32HsX@cluster0-ih4z3.mongodb.net/cafe';
+    urlDB = process.env.MONGO_URI;
 }
 
 process.env.URLDB = urlDB;
