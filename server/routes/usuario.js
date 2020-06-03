3a280b6055fb9139f50815ba6c43476150aa9751
Maxi <maxipg@gmail.com>
2020-06-03 15:34:44 -0300

    Mongo Altas configurado

--- server/routes/usuario.js
+++ server/routes/usuario.js
@@ -6,7 +6,39 @@ const app = express();
 
 
 app.get('/usuario', (req, res) => {
-    res.json('Get Usuario')
+    // parametros de URL
+    let desde = req.query.desde || 0;
+    let limite = req.query.limite || 5;
+    //lo convertimos a numero pq viene como String    
+    desde = Number(desde);
+    limite = Number(limite);
+    //filtro por estado
+    let filtro = {
+        estado: true
+    }
+    //Obtenemos los datos con los filtros aplicados
+    //el segundo parametro son los campos que voy a obtener
+    Usuario.find(filtro, 'nombre email img estado google')
+        .limit(limite)
+        .skip(desde)
+        .exec((err, usuarios) => {
+            if (err) {
+                return res.json(400, {
+                    ok: false,
+                    err
+                })
+            }
+            // Devolvemos la cantidad total de registro de la base
+            Usuario.countDocuments(filtro, (err, cantidad) => {
+                res.json({
+                    ok: true,
+                    cantidad,
+                    usuarios
+                })
+            })
+        })
+
+
 })
 
 //Insertamos datos en la coleccion Usuarios
@@ -43,13 +75,16 @@ app.post('/usuario', (req, res) => {
 // Modificar datos del usuario
 app.put('/usuario/:id', (req, res) => {
     let id = req.params.id;
-
+    let options = {
+        new: true,
+        runValidators: true
+    };
     //Usamos el metodo PICK de underscore que nos devuelve
     //el mismo objeto con las propiedades que enviamos como 2do parametro
     let body = _.pick(req.body, ['nombre', 'email', 'img', 'rol', 'estado']);
 
     //Actualizamos en registro en la base de datos
-    Usuario.findByIdAndUpdate(id, body, { new: true, runValidators: true }, (err, usuarioDB) => {
+    Usuario.findByIdAndUpdate(id, body, options, (err, usuarioDB) => {
 
         if (err) {
             return res.json(400, {
@@ -65,8 +100,58 @@ app.put('/usuario/:id', (req, res) => {
     })
 })
 
-app.delete('/usuario', (req, res) => {
-    res.json('Delete Usuario')
+app.delete('/usuario/:id', (req, res) => {
+
+    let id = req.params.id;
+    //Cambio de estado
+    let body = {
+        estado: false
+    }
+
+    let options = {
+        new: true
+    }
+    //Eliminar registro de forma logica
+    Usuario.findByIdAndUpdate(id, body, options, (err, usuarioDB) => {
+
+        if (err) {
+            return res.json(400, {
+                ok: false,
+                err
+            })
+        }
+
+        res.json(200, {
+            ok: true,
+            usuarioDB
+        })
+    })
+
+    /*
+    //Eliminar registro de forma fisica
+    Usuario.findByIdAndRemove(id, (err, usuarioEliminado)=>{
+        if (err) {
+            return res.json(400, {
+                ok: false,
+                err
+            })
+        }
+
+        if (!usuarioEliminado) {
+            return res.json(400, {
+                ok: false,
+                err:{
+                    message: 'Usuario no encontrado'
+                }
+            })
+        }
+
+        res.json(200, {
+            ok: true,
+            usuario: usuarioEliminado
+        })
+    })
+    */
 })
 
 module.exports = app;
--- server/server.js
+++ server/server.js
@@ -9,7 +9,7 @@ const app = express();
 
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }))
- 
+
 // parse application/json
 app.use(bodyParser.json())
 
@@ -17,15 +17,16 @@ app.use(bodyParser.json())
 app.use(require('./routes/usuario'))
 
 // Conectamos con MongoDB
-mongoose.connect('mongodb://localhost:27017/cafe', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-}, (err, resp)=>{
+mongoose.connect(process.env.URLDB, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true
+  }, (err, resp) => {
     if (err) throw err;
     console.log('Base de datos ONLINE!!');
-});
+  });
 
 //Levantamos el Server
 app.listen(process.env.PORT, () => {
-    console.log('Escuchando puerto', process.env.PORT);
+  console.log('Escuchando puerto', process.env.PORT);
 });
